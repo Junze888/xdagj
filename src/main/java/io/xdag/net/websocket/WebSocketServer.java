3e29a5c08bc71a5f4a0255f1a2384bbb0b2b0b01
June <2571240520@qq.com>
2023-11-28 22:42:05 +0800

    test websocketServer.

--- src/main/java/io/xdag/net/websocket/WebSocketServer.java
+++ src/main/java/io/xdag/net/websocket/WebSocketServer.java
@@ -2,7 +2,10 @@ package io.xdag.net.websocket;
 
 import io.netty.channel.Channel;
 import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
 import io.netty.channel.EventLoopGroup;
+import io.netty.channel.epoll.EpollEventLoopGroup;
+import io.netty.channel.kqueue.KQueueEventLoopGroup;
 import io.netty.channel.socket.SocketChannel;
 import io.netty.handler.codec.http.HttpObjectAggregator;
 import io.netty.handler.codec.http.HttpServerCodec;
@@ -11,31 +14,43 @@ import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
 import io.netty.handler.logging.LoggingHandler;
 import java.util.Objects;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import io.xdag.utils.NettyUtils;
+import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.SystemUtils;
+import org.apache.commons.lang3.concurrent.BasicThreadFactory;
 
 
 @Slf4j
+@Getter
 public class WebSocketServer {
     private  final String ClientHost;
     private final String ClientTag;
     private final int ServerPort;
-    private final EventLoopGroup bossGroup;
-    private final EventLoopGroup workerGroup;
+    private EventLoopGroup bossGroup;
+    private EventLoopGroup workerGroup;
     private Channel webSocketChannel;
-
+    private final ExecutorService mainExecutor = Executors.newSingleThreadExecutor(new BasicThreadFactory.Builder()
+            .namingPattern("WebsocketServer-Main-Thread-%d")
+            .daemon(true)
+            .build());
     public WebSocketServer(String clientHost, String tag, int port) {
         this.ClientHost = clientHost;
         this.ClientTag = tag;
         this.ServerPort = port;
-        this.bossGroup = new NioEventLoopGroup();
-        this.workerGroup = new NioEventLoopGroup();
     }
 
 
     public void start() throws InterruptedException {
         log.info("Pool WebSocket enabled");
-        ServerBootstrap b = new ServerBootstrap();
-        b.group(bossGroup, workerGroup)
+        mainExecutor.execute(() ->{
+                bossGroup = new NioEventLoopGroup();
+                workerGroup = new NioEventLoopGroup();
+                ServerBootstrap b = new ServerBootstrap();
+                b.group(bossGroup, workerGroup)
                 .channel(NioServerSocketChannel.class)
                 .childHandler(new ChannelInitializer<SocketChannel>() {
                     @Override
@@ -46,7 +61,18 @@ public class WebSocketServer {
                         ch.pipeline().addLast("handler", new PoolHandShakeHandler(ClientHost, ClientTag, ServerPort));//pool handler write by ourselves
                     }
                 });//initialize worker Group
-        webSocketChannel = b.bind("localhost",ServerPort).channel();
+            try {
+                // 绑定端口并启动服务器
+                webSocketChannel = b.bind("localhost", ServerPort).channel();
+                webSocketChannel.closeFuture().sync();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } finally {
+                // 关闭 EventLoopGroup
+                bossGroup.shutdownGracefully();
+                workerGroup.shutdownGracefully();
+            }
+        });
     }
 
     public void stop() {
@@ -58,6 +84,7 @@ public class WebSocketServer {
         }
         this.bossGroup.shutdownGracefully();
         this.workerGroup.shutdownGracefully();
+        mainExecutor.shutdown();
     }
 }
 
